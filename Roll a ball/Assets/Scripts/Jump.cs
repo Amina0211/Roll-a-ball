2c86326b0a121063348daaedf6a77bb16697f5fb
Amina0211 <ahodzic@bennington.edu>
2016-03-07 22:01:45 -0500

    Jump Addition

---
+++ Roll a ball/Assets/Scripts/Jump.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Jump : MonoBehaviour {
+	public float thrust;
+	private Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+	void FixedUpdate() {
+		if (Input.GetButtonDown ("Jump")) {
+			Vector3 vector = new Vector3 (0.0f, 1.0f, 0.0f);
+			rb.AddForce (vector * thrust);
+		}
+	}
+}
